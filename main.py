b105eda8c6d87463058e82419bc35bd20a33e380
Pavan Katta <pavanyellow@gmail.com>
2024-06-14 22:00:23 +0530

    a

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ log = logging.getLogger(__name__)
 
 
 args = dotdict({
-    'numIters': 1,
-    'numEps': 1,              # Number of complete self-play games to simulate during a new iteration.
+    'numIters': 1000,
+    'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 15,        #
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
